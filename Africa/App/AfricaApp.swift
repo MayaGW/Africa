a2d78d819fe90263f614fe0b6c9064ea7b5d017a
Maya Ghamloush <maya.ghamloushe@gmail.com>
2024-02-21 19:40:42 +0100

    Project Settings

---
+++ Africa/App/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Maya Ghamloush on 21/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
